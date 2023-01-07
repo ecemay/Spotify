ac5911c0c59c3585fa9ea9a664cc9ff71d2630b8
ecemay <112403291+ecemay@users.noreply.github.com>
2023-01-07 21:27:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ date = input("Enter a date as YYYY-MM-DD:\n")
 billboard_playlist = BillboardToSpotifyt(user_id=USER_ID,date=date, client_secret=CLIENT_SECRET,client_id=CLIENT_ID,redirect_uri=REDIRECT_URI)
 
 ## To reach token you should call the function of request_user_authorization. This process has two step. 1. Go to link
-#anc confirm authorization. 2. paste the code in the url code= part.As a result of this two-step process,
-# the authorization process is completed and the token is accessed.
+#and confirm authorization. 2. Paste the code in the url code= part.As a result of this two-step process,
+# the authorization process will be completed and the token will be accessed.
 billboard_playlist.request_user_authorization()
 
 ## create a private spotify playlist named by the entered date by calling the function creation_playlist
